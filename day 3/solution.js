00d854d19bb2caa5c8714b6d50848418b7ede96c
unknown <joranminjon@gmail.com>
2020-12-12 12:31:17 +0100

    Solution day 3 part 2

--- day 3/solution.js
+++ day 3/solution.js
@@ -2,9 +2,9 @@ const inputFile = require('./input.js');
 
 const parsedInput = inputFile.input.split('\n');
 
-function solvePart1(input) {
-    rowLength = input[0].length;
+const rowLength = parsedInput[0].length; 
 
+function solvePart1(input) {
     let treesHit = 0;
     let xValue = 0;
     for (var i = 0; i < input.length - 1; i++) {
@@ -15,4 +15,46 @@ function solvePart1(input) {
     }
     return treesHit;
 }
+
+
+function solvePart2(input) {
+    xValues = new Array(5).fill(0);
+    trees = new Array(5).fill(0);
+
+    let treesHit = 0;
+    let xValue = 0;
+    for (var i = 0; i < input.length; i++) {
+        for (var j = 0; j < xValues.length; j++) {
+            if (input[i].charAt(xValues[j]) == '#') {
+                // We need to skip counting trees for j == 4 on uneven rows
+                if (j != 4 || i % 2 == 0) {
+                    trees[j]++;
+                }
+            }
+        }
+        updateXValues(i);
+    }
+    return trees;
+}
+
+function updateXValues(rowNumber) {
+    // [0] = Right 1, down 1.
+    // [1] = Right 3, down 1. (This is the slope you already checked.)
+    // [2] = Right 5, down 1.
+    // [3] = Right 7, down 1.
+    // [4] = Right 1, down 2.
+    xValues[0] = (xValues[0] + 1) % rowLength;
+    xValues[1] = (xValues[1] + 3) % rowLength;
+    xValues[2] = (xValues[2] + 5) % rowLength;
+    xValues[3] = (xValues[3] + 7) % rowLength;
+    if (rowNumber % 2 == 1) {
+        // Update the xValue on uneven rows
+        xValues[4] = (xValues[4] + 1) % rowLength;
+    }
+}
+
 console.log(solvePart1(parsedInput));
+
+console.log(solvePart2(parsedInput).reduce((total, number) => {
+    return total * number;
+}));
