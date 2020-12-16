1e068d94ea2dd760007ec7014e64283b72670cd7
unknown <joranminjon@gmail.com>
2020-12-16 18:34:34 +0100

    Rename parsedInput

--- day 4/solution.js
+++ day 4/solution.js
@@ -1,6 +1,6 @@
 const inputFile = require('./input');
 
-const parsedInput = inputFile.input.split("\n\n");
+const passports = inputFile.input.split("\n\n");
 
 const neededFields = ['ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'hgt'] // every field except 'cid'
 
@@ -12,10 +12,10 @@ function solvePart1(input) {
     return validPassports;
 }
 
-function solvePart2(input) {    
+function solvePart2(passports) {    
     validPassports = 0;
-    for (var i = 0; i < input.length; i++) {
-        const fields = input[i].split(/\s/g).map(field => field.substring(0, 3));
+    for (var i = 0; i < passports.length; i++) {
+        const fields = passports[i].split(/\s/g).map(field => field.substring(0, 3));
         if (isPassportValidPart1(fields)) {
             isPassportValidPart2(fields) && validPassports++;
         }
@@ -31,6 +31,6 @@ function isPassportValidPart2(passport) {
 
 }
 
-console.log(solvePart1(parsedInput));
-console.log(solvePart2(parsedInput));
+console.log(solvePart1(passports));
+console.log(solvePart2(passports));
 
