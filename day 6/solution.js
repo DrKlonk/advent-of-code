71785e086979ca670c6486f205b020d25f0b88c3
unknown <joranminjon@gmail.com>
2020-12-18 11:06:50 +0100

    Solution day 6

---
+++ day 6/solution.js
@@ -0,0 +1,46 @@
+const groups = require('./input').input.split('\n\n');
+
+function solvePart1(groups) {
+    let totalSum = 0;
+    for (var i = 0; i < groups.length; i++) {
+        totalSum += calcUniqueChars(groups[i]).size;
+    }
+    return totalSum;
+}
+
+function solvePart2(groups) {
+    let totalSum = 0;
+    for (var i = 0; i < groups.length; i++) {
+        totalSum += calcCommonChars(groups[i]);
+    }
+    return totalSum;
+}
+
+function calcUniqueChars(group) {
+    uniqueChars = new Set();
+    for (var i = 0; i < group.length; i++) {
+        const currentChar = group[i];
+        if (uniqueChars.has(currentChar) || currentChar === '\n') {
+            continue;
+        } 
+        uniqueChars.add(currentChar);
+    }
+    return uniqueChars;
+}
+
+function calcCommonChars(group) {
+    const answers = group.split('\n');
+    let commonChars = calcUniqueChars(answers[0]);
+    for (var i = 0; i < answers.length; i++) {
+        const currentAnswer = answers[i];
+        commonChars.forEach(char => {
+            if (currentAnswer.indexOf(char) == -1) {
+                commonChars.delete(char);
+            }
+        });
+    }
+    return commonChars.size;
+}
+
+console.log(solvePart1(groups));
+console.log(solvePart2(groups));
