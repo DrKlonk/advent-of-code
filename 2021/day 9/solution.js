1cea389e61a851b0180c77b3af72d0d80f7f7afa
joranminjon <joranminjon@gmail.com>
2021-12-21 06:15:26 +0100

    day 9 part 2

--- 2021/day 9/solution.js
+++ 2021/day 9/solution.js
@@ -31,4 +31,59 @@ function isLowPoint(input, row, col) {
   return true
 }
 
-console.log(solution1(parsedInput))
+function solution2(input) {
+  const isVisited = new Array(input.length)
+    .fill(0)
+    .map(() => new Array(input[0].length).fill(false))
+
+  const basins = []
+
+  const toCheckQueue = []
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[0].length; j++) {
+      toCheckQueue.push([i, j])
+      let basinSize = 0
+
+      while (toCheckQueue.length > 0) {
+        const [row, col] = toCheckQueue.pop()
+
+        if (isVisited[row][col] || input[row][col] == "9") {
+          continue
+        }
+        isVisited[row][col] = true
+
+        basinSize++
+        addNeighbors(input, row, col, isVisited, toCheckQueue)
+      }
+      if (basinSize > 0) {
+        basins.push(basinSize)
+      }
+    }
+  }
+
+  return basins
+    .sort((a, b) => a - b)
+    .splice(-3)
+    .reduce((total, basin) => {
+      return total * basin
+    })
+}
+
+function addNeighbors(input, x, y, toBeChecked, toCheckQueue) {
+  if (x > 0 && !toBeChecked[x - 1][y]) {
+    toCheckQueue.push([x - 1, y])
+  }
+  if (x < input.length - 1 && !toBeChecked[x + 1][y]) {
+    toCheckQueue.push([x + 1, y])
+  }
+  if (y > 0 && !toBeChecked[x][y - 1]) {
+    toCheckQueue.push([x, y - 1])
+  }
+  if (y < input[0].length - 1 && !toBeChecked[x][y + 1]) {
+    toCheckQueue.push([x, y + 1])
+  }
+  return toCheckQueue
+}
+
+console.log(solution2(parsedInput))
